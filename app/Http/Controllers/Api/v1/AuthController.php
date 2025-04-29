3ba67839eaafd2733e8aebbdad83c38f65b1ca37
Cedric Ko <yacmov@gmail.com>
2025-04-30 01:01:18 +0800

    feat: implements Auth API Controller register with user registration and token generation.
    - This includes validation of registration data, handling of profile photos, and generation of a JWT token for the newly registered user.
    - It also ensures a default name is created if no name is provided.

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -10,6 +10,47 @@ use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
+
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'given_name' => ['required_without:family_name', 'min:2', 'max:255', 'string'],
+            'family_name' => ['required_without:given_name', 'min:2', 'max:255', 'string'],
+            'name' => ['nullable', 'min:2', 'max:255', 'string'],
+            'preferred_pronouns' => ['required', 'min:2', 'max:10', 'string'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', 'min:4', 'max:255', Password::defaults()],
+            'profile_photo' => ['nullable', 'string', 'min:4', 'max:255'],
+        ]);
+
+        /**
+         * Check if name is not provided use given / family name as default
+         */
+        if (empty($request->name)) {
+            if ($validated['given_name'] != null) {
+                $validated['name'] = $validated['given_name'];
+            } else {
+                $validated['name'] = $validated['family_name'];
+            }
+        }
+
+        if ($request->hasFile('profile_photo')) {
+            $path = $request->file('profile_photo')->store('profile_photos', 'public');
+            $validated['profile_photo'] = $path;
+        } else {
+            $validated['profile_photo'] = "avatar.png";
+        }
+
+        $user = User::create($validated);
+
+        $token = $user->createToken($request->email);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ];
+    }
+
     public function login(Request $request)
     {
         $request->validate([
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
  *  - Index, create, Show, Update, Destroy (Auth required)
  */
 Route::group(['prefix'=> 'auth'], function () {
+  Route::post('/register', [AuthController::class, 'register']);
   Route::post('/login', [AuthController::class, 'login']);
   Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
