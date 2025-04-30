33ff198345b854a4807af16cb8a61314ee3ca48c
Cedric Ko <yacmov@gmail.com>
2025-04-30 10:00:52 +0800

    fix: Update initial test cases to match the project.
    - Updated new packages.

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -8,12 +8,14 @@ test('registration screen can be rendered', function () {
 
 test('new users can register', function () {
     $response = $this->post('/register', [
+        'given_name' => 'Test Given',
         'name' => 'Test User',
+        'preferred_pronouns' => 'test',
         'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
+        'password' => 'Password1',
+        'password_confirmation' => 'Password1',
     ]);
-
+    
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
 });
